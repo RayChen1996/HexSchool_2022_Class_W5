8f14c0dfe7f90ed128a91f11d03bd3a807b290e1
RayChen <ray.10315332@gmail.com>
2022-08-06 20:20:12 +0800

    Update 2022-08-06T12:20:10.534Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,14 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
   $('.tab').on('click', function () {
     console.log("click one");
     $('.tab').removeClass('active');
+    $(this).addClass('active');
+  });
+  $('.mnuBar .tab').on('click', function () {
+    console.log("click one");
+    $('.mnuBar .tab').removeClass('active');
     $(this).addClass('active');
   });
 });
