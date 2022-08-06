d54ba4c53f90b7da24b7da8564beffc67b9494b8
RayChen <ray.10315332@gmail.com>
2022-08-06 15:31:28 +0800

    Week5 切版任務_使用SCSS 初版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,4 +2,15 @@ console.log('Hello!');
 
 $(document).ready(() => {
   console.log('HesSchool Hello!');
+
+
+
+
+  $('.tab').on('click', function() {
+    console.log("click one")
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+  });
 });
+
+
