b08233372735c3f31beeea7bd4c2dadbb46ccd1d
RayChen <ray.10315332@gmail.com>
2022-08-06 15:25:25 +0800

    Update 2022-08-06T07:25:23.992Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,10 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+  $('.tab').on('click', function () {
+    console.log("click one");
+    $('.tab').removeClass('active');
+    $(this).addClass('active');
+  });
 });
 //# sourceMappingURL=all.js.map
